8b9053b293c6539698d9fda5923ba72fbcfefebf
Istvan888 <marai.istvanp@gmail.com>
2022-08-30 13:18:03 +0200

    Done

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,19 @@
+// 1. Írj egy függvény kifejezést `removeDuplicatesFromArray` néven, ami a paraméterként megadott tömbből eltávolítja a duplikált elemeket! A tömb csak primítív értékeket tartalmaz! 
+// A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
+
+'use strict';
+
+const removeDuplicatesFromArray = (array) => array.filter((c, index) =>
+    array.indexOf(c) === index);
+
+removeDuplicatesFromArray([1, 2, 3, 4, 1, 2]);
+
+
+
+
+
+
+
+
+
 export default removeDuplicatesFromArray;
--- 08-other/exercises/02/solution/js/HU.js
+++ 08-other/exercises/02/solution/js/HU.js
@@ -1,1 +1,25 @@
-export default HU;
+// 2. Készíts egy `HU` nevű objektumot, aminek az alábbi propertijei vannak:
+// - `date()`: Visszaadja a paraméterként megadott dátumot a magyar dátumírás szabályainak megfelelően
+// - `currency()`: Visszaadja a paraméterként megadott számot a magyar pénzformátumnak megfelelően a Ft végződéssel együtt
+// - `list()`: A paraméterként kapott string tömböt visszaadja az alábbi formában: első, második és harmadik" (Tehát vesszőkkel elválasztva az utolsó elem előtt az és szóval)
+
+'use strict'
+
+const HU = {
+  date(date) {
+    return new Intl.DateTimeFormat("hu-HU").format(date);
+  },
+  currency(value) {
+    return new Intl.NumberFormat("hu-HU", {
+      style: "currency",
+      currency: "HUF",
+    }).format(value);
+  },
+  list(stringArr) {
+    const lastEl = stringArr.pop();
+    return stringArr.join(", ").concat(` és ${lastEl}`);
+  },
+};
+
+
+export default HU
